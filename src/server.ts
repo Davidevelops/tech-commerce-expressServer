975ce7e434bbd833a86abcbdb9727db887a76fb3
David james <davidjamesdjdc@gmail.com>
2025-07-10 20:24:58 +0800

    port changes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { connectToDB } from "./config/db";
 import app from "./app";
 
 connectToDB().then(() => {
-  app.listen(process.env.PORT, () =>
+  app.listen(process.env.PORT || 5000, () =>
     console.log("server running on port: ", process.env.PORT)
   );
 });
